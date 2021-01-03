115306d00b8b7527e9acab5ce02fa4120d7a412a
brownspunbear <benjaminrshill@hotmail.com>
2021-01-03 09:25:43 +0000

    highlight current nav location

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,42 +1,51 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import './nav.css';
 
-export default function Nav(props) {
-    return (
-        <nav>
-            <Link to='/Items' className='menu-item'>
-                <div className='menu-icon'>
-                    +
-                </div>
-                <div className='menu-text'>
-                    Items
-                </div>
-            </Link>
-            <Link to='/Arrange' className='menu-item'>
-                <div className='menu-icon'>
-                    <span></span>
-                </div>
-                <div className='menu-text'>
-                    Arrange
-                </div>
-            </Link>
-            <Link to='/Week' className='menu-item'>
-                <div className='menu-icon'>
-                    <span className='done'></span>
-                </div>
-                <div className='menu-text'>
-                    This week
-                </div>
-            </Link>
-            <Link to='/History' className='menu-item'>
-                <div className='menu-icon'>
-                    &#8592;
-                </div>
-                <div className='menu-text'>
-                    History
-                </div>
-            </Link>
-        </nav>
-    );
+class Nav extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <nav>
+                <Link to='/Items' className={'menu-item ' + (this.props.location.pathname === '/Items' ? 'here' : '')}>
+                    <div className='menu-icon'>
+                        +
+                    </div>
+                    <div className='menu-text'>
+                        Items
+                    </div>
+                </Link>
+                <Link to='/Arrange' className={'menu-item ' + (this.props.location.pathname === '/Arrange' ? 'here' : '')}>
+                    <div className='menu-icon'>
+                        <span></span>
+                    </div>
+                    <div className='menu-text'>
+                        Arrange
+                    </div>
+                </Link>
+                <Link to='/Week' className={'menu-item ' + (this.props.location.pathname === '/Week' ? 'here' : '')}>
+                    <div className='menu-icon'>
+                        <span className='done'></span>
+                    </div>
+                    <div className='menu-text'>
+                        This week
+                    </div>
+                </Link>
+                <Link to='/History' className={'menu-item ' + (this.props.location.pathname === '/History' ? 'here' : '')}>
+                    <div className='menu-icon'>
+                        &#8592;
+                    </div>
+                    <div className='menu-text'>
+                        History
+                    </div>
+                </Link>
+            </nav>
+        );
+    }
 }
+
+export default withRouter(Nav);
